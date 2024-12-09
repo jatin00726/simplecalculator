7443cdabb748f359a5547ccb6c48f72aa757e9a1
Dilip Samanta <dilip.samanta@gmail.com>
2024-12-09 23:44:05 +0530

    First Checkin

---
+++ amazingcalculator.business/Math.cs
@@ -0,0 +1,24 @@
+﻿namespace amazingcalculator.business
+{
+    public class Math
+    {
+        public int Add(int x, int y) { return x + y; }
+        public int Sub(int x, int y) {return x - y; }
+
+        public int Mul(int x, int y) {return (x * y); }
+
+        public int Div(int x, int y) 
+        {
+            int z = 0;
+            try
+            {
+                z = x / y;
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+
+            return z;
+        }
+    }
+}
